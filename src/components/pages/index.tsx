84ba6746d74d7d81539eec3a5eb5c4548319c0de
Jose Wilson <wilsoncx@gmail.com>
2020-08-01 15:48:55 -0300

    chore: add git commit msg linter

---
+++ src/components/pages/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const Pages: React.FC = () => {
+  return (
+    <div>
+      <Typography variant="h1" noWrap>
+        Vamos começar
+      </Typography>
+    </div>
+  );
+};
+
+export default Pages;
