2da0de4378f4bcb8135248a311b7fe63240df032
Jose Wilson <wilsoncx@gmail.com>
2020-08-02 13:34:13 -0300

    chore: add react-router-dom

---
+++ src/components/pages/admin/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+const admin: React.FC = () => {
+  return (
+    <Typography variant="h1" noWrap>
+      rotas Admin
+    </Typography>
+  );
+};
+
+export default admin;
